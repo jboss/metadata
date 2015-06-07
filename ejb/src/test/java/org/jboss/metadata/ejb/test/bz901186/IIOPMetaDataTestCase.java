36f4bce857be53955c31863dc21c43290a81772f
Weston M. Price <wprice@redhat.com>
2015-06-06 21:24:22 -0400

    WIP Adding IIOP related support files for BZ-901186

---
+++ ejb/src/test/java/org/jboss/metadata/ejb/test/bz901186/IIOPMetaDataTestCase.java
@@ -0,0 +1,17 @@
+package org.jboss.metadata.ejb.test.bz901186;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by wmprice on 6/6/15.
+ */
+public class IIOPMetaDataTestCase {
+
+    @Test
+    public void testIIOPParser() throws Exception {
+    }
+
+
+
+}
